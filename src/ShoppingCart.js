26e12c0d382a332b54122e7522424e5d5bff5ae4
ARI DINAR <dinarari@gmail.com>
2019-01-21 09:22:50 +0100

    clear the code

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -22,17 +22,4 @@ class ShoppingCart {
     }
 }
 
-// const cartTest = new ShoppingCart()
-// cartTest.addItem("item 1", 1, 15.5)
-// cartTest.addItem("item 2", 2, 3)
-// cartTest.addItem("item 3", 10, 2)
-// cartTest.addItem("item 4", 0, 2)
-// cartTest.addItem("item 5", 2, 0)
-// console.log(cartTest.total())
-// cartTest.addItem("Name of the item", 1, 99.99)
-// console.log(cartTest.getItems())
-// cartTest.clear()
-// console.log(cartTest.getItems())
-
-
 module.exports.ShoppingCart = ShoppingCart
